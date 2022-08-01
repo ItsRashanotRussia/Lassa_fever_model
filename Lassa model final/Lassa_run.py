f7d821ab2252c42c80acbc709f17418e5dd4540a
Rasha Boura <Rashaboura123@gmail.com>
2022-08-01 16:36:05 -0700

    Update Lassa_run.py

--- Lassa model final/Lassa_run.py
+++ Lassa model final/Lassa_run.py
@@ -1,6 +1,10 @@
-
-
-
+"""
+Author      : Rasha Boura
+PI          : Dr. Sampson Akwafuo
+File Name   : Lassa_model.py
+Date        : August 1st, 2022
+"""
 from Lassa_server import server
+
 server.port = 8521
 server.launch()
